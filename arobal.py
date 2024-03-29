6b721fb5b0b8e04b961a276dcfd3b0b91a7e833e
namber0 <namnb2402@gmail.com>
2024-03-29 20:04:27 +0700

    Fix double quote around strings and brackets around list when printing

--- arobal.py
+++ arobal.py
@@ -1204,6 +1204,9 @@ class String(Value):
         copy.set_context(self.context)
 
         return copy
+    
+    def __str__(self) -> str:
+        return self.value
 
     def __repr__(self):
         return f'"{self.value}"'
@@ -1475,6 +1478,9 @@ class List(Value):
         copy.set_context(self.context)
         
         return copy
+    
+    def __str__(self) -> str:
+        return ", ".join([str(x) for x in self.elements])
 
     def __repr__(self):
         return f'[{", ".join([str(x) for x in self.elements])}]'
